883b668d73a4acd17e446a36dee271801855751e
DualCoreSolutions <dualcoresolutions.tech@gmail.com>
2026-09-02 21:36:22 -0400

    arrumando bug das culturas

--- logica/motor_agricultura.py
+++ logica/motor_agricultura.py
@@ -7,22 +7,22 @@ class MotorAgricultura:
     # ⚙️ PAINEL DE CONFIGURAÇÃO - CULTURAS (TEMPO E ÁGUA)
     # ==========================================
     CONFIG_CULTIVOS = {
-        'feijao':   {'dias_semente': 3,  'dias_broto': 8,   'dias_colheita': 20,   'agua_necessaria': 30},
-        'melancia': {'dias_semente': 3,  'dias_broto': 10,  'dias_colheita': 25,   'agua_necessaria': 30},
-        'milho':    {'dias_semente': 4,  'dias_broto': 10,  'dias_colheita': 25,   'agua_necessaria': 40},
-        'soja':     {'dias_semente': 5,  'dias_broto': 12,  'dias_colheita': 30,   'agua_necessaria': 50},
-        'arroz':    {'dias_semente': 5,  'dias_broto': 15,  'dias_colheita': 35,   'agua_necessaria': 80},
-        'pimenta':  {'dias_semente': 5,  'dias_broto': 15,  'dias_colheita': 40,   'agua_necessaria': 40},
-        'algodao':  {'dias_semente': 6,  'dias_broto': 20,  'dias_colheita': 45,   'agua_necessaria': 60},
-        'mandioca': {'dias_semente': 8,  'dias_broto': 25,  'dias_colheita': 60,   'agua_necessaria': 20},
-        'banana':   {'dias_semente': 10, 'dias_broto': 30,  'dias_colheita': 80,   'agua_necessaria': 50},
+        'feijao':   {'dias_semente': 10, 'dias_broto': 30,  'dias_colheita': 80,   'agua_necessaria': 30},
+        'melancia': {'dias_semente': 10, 'dias_broto': 30,  'dias_colheita': 85,   'agua_necessaria': 30},
+        'milho':    {'dias_semente': 10, 'dias_broto': 30,  'dias_colheita': 90,   'agua_necessaria': 40},
+        'tomate':   {'dias_semente': 10, 'dias_broto': 30,  'dias_colheita': 90,   'agua_necessaria': 40},
+        'soja':     {'dias_semente': 15, 'dias_broto': 40,  'dias_colheita': 100,  'agua_necessaria': 50},
+        'arroz':    {'dias_semente': 15, 'dias_broto': 40,  'dias_colheita': 110,  'agua_necessaria': 80},
+        'pimenta':  {'dias_semente': 15, 'dias_broto': 40,  'dias_colheita': 120,  'agua_necessaria': 40},
+        'algodao':  {'dias_semente': 20, 'dias_broto': 50,  'dias_colheita': 150,  'agua_necessaria': 60},
+        'mandioca': {'dias_semente': 30, 'dias_broto': 80,  'dias_colheita': 240,  'agua_necessaria': 20},
+        'banana':   {'dias_semente': 30, 'dias_broto': 100, 'dias_colheita': 300,  'agua_necessaria': 50},
         'cana':     {'dias_semente': 30, 'dias_broto': 120, 'dias_colheita': 360,  'agua_necessaria': 50},
         'cafe':     {'dias_semente': 45, 'dias_broto': 150, 'dias_colheita': 365,  'agua_necessaria': 40},
-        'cupuacu':  {'dias_semente': 20, 'dias_broto': 60,  'dias_colheita': 150,  'agua_necessaria': 60},
-        'cacau':    {'dias_semente': 20, 'dias_broto': 60,  'dias_colheita': 150,  'agua_necessaria': 60},
-        'acai':     {'dias_semente': 25, 'dias_broto': 70,  'dias_colheita': 180,  'agua_necessaria': 70},
-        'tomate':   {'dias_semente': 3,  'dias_broto': 10,  'dias_colheita': 25,   'agua_necessaria': 40},
-        'abacaxi':  {'dias_semente': 10, 'dias_broto': 30,  'dias_colheita': 400,  'agua_necessaria': 40}
+        'abacaxi':  {'dias_semente': 45, 'dias_broto': 150, 'dias_colheita': 400,  'agua_necessaria': 40},
+        'cacau':    {'dias_semente': 60, 'dias_broto': 200, 'dias_colheita': 500,  'agua_necessaria': 60},
+        'cupuacu':  {'dias_semente': 90, 'dias_broto': 250, 'dias_colheita': 730,  'agua_necessaria': 60},
+        'acai':     {'dias_semente': 90, 'dias_broto': 250, 'dias_colheita': 730,  'agua_necessaria': 70}
     }
 
 
